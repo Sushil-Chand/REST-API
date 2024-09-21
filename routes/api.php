a4b275e9bb19e2421bc5ee2c33318ba6ded9400a
Sushil Chand <sushilchand662@gmail.com>
2024-09-21 22:49:08 +0545

    make api route post

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    return response()->json('hello');
+});
+
+Route::get('/posts',[PostController::class,'index']);
+Route::post('/posts',[PostController::class,'store']);
+Route::get('/posts/{post}',[PostController::class,'show']);
